6f5e2e38e1b6e0f964ec684f8854ccaa83891650
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:36 +0200

    object to class

--- src/test/scala/nl/biopet/tools/ncbireporttocontigmap/NcbiReporttoContigMapTest.scala
+++ src/test/scala/nl/biopet/tools/ncbireporttocontigmap/NcbiReporttoContigMapTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.ncbireporttocontigmap
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object NcbiReporttoContigMapTest extends BiopetTest {
+class NcbiReporttoContigMapTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
